8b689a4c3cf147ace3928e3e0c462b6345d1a4b8
kamalcodezen <kamaluddin7908@gmail.com>
2026-05-11 23:44:27 +0530

    navabr creat

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,46 @@
+import logoImg from "@/assets/images/logo.png";
+import { FaGithub } from "react-icons/fa";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  const navItems = [
+    {
+      path: "/",
+      text: "Home",
+    },
+    {
+      path: "/apps",
+      text: "Apps",
+    },
+    {
+      path: "/installedApps",
+      text: "Installation",
+    },
+    {
+      path: "/dashboard",
+      text: "Dashboard",
+    },
+  ];
+
+  return (
+    <nav className=" shadow">
+      <div className="flex justify-between gap-4 items-center  py-[8px] container mx-auto">
+        <Image src={logoImg} alt="" width={30} height={30} />
+        <ul className="flex justify-between gap-2 items-center">
+          {navItems.map((item, index) => (
+            <Link key={index} href={item.path}>
+              {item.text}
+            </Link>
+          ))}
+        </ul>
+        <button className="btn bg-purple-500 text-white">
+          <FaGithub />
+          Contribute
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
