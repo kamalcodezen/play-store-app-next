411254495bf35f8ab08f6b6beb7ba6ab5a669c29
kamalcodezen <kamaluddin7908@gmail.com>
2026-05-12 00:50:45 +0530

    banner and stats section created

--- src/components/homePage/Stats.jsx
+++ src/components/homePage/Stats.jsx
@@ -1,1 +1,11 @@
+import React from "react";
 
+const Stats = () => {
+  return (
+    <div className="bg-linear-to-br from-indigo-500 via-cyan-400 to-purple-500 h-[40vh] font-bold text-5xl text-center flex items-center justify-center">
+      Stats section
+    </div>
+  );
+};
+
+export default Stats;
